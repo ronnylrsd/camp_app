b6732345b3260159ec5c5aa5b6d0025b6828e1a1
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2024-01-21 17:14:04 -0300

    Update: button pointer

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ type ButtonProps = {
     type: "button" | "submit",
     title: string,
     icon?: string,
-    variant: "btn_dark_green"
+    variant: "btn_dark_green" | "btn_green" | "btn_white"
 }
 
 const Button = ({type, title, icon, variant}: ButtonProps) => {
@@ -13,8 +13,8 @@ const Button = ({type, title, icon, variant}: ButtonProps) => {
         className={`flexCenter gap-3 ${variant} py-2 px-4 rounded-full border`}
         type={type}
     >
-        {icon && <Image src={icon} alt="icon" width={24} height={24} />}
-        <label className='bold-16 whitespace-nowrap'>{title}</label>
+        {icon && <Image src={icon} alt="icon" width={24} height={24} className='cursor-pointer' />}
+        <label className='bold-16 whitespace-nowrap cursor-pointer'>{title}</label>
     </button>
   )
 }
