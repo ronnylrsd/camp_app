df641d9e021c7fc783714fd384bc24775aed43d8
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2024-01-21 16:44:25 -0300

    Feat: add menu logic

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,48 @@
+"use client"
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleCloseMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
-      <ul className="hidden h-full gap-12 lg:flex">
-        {NAV_LINKS.map((link) => (
-          <Link
-            href={link.href}
-            key={link.key}
-            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
-          >
-            {link.label}
-          </Link>
-        ))}
-      </ul>
+      <div className={`fixed inset-0 flex items-center justify-center ${
+          isMenuOpen ? "block" : "hidden"
+        } lg:hidden bg-white bg-opacity-90 z-50`}>
+        <div className="bg-gray-100 border-2 border-gray-300 p-4 rounded-md">
+          <ul className="flex flex-col space-y-4">
+            <Image
+              src={"/close.svg"}
+              alt="close menu"
+              width={24}
+              height={24}
+              className="self-end cursor-pointer border-2 border-white color"
+              onClick={handleCloseMenu}
+            />
+            {NAV_LINKS.map((link) => (
+              <Link
+                href={link.href}
+                key={link.key}
+                className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+                onClick={handleCloseMenu}
+              >
+                {link.label}
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </div>
       <div className="lg:flexCenter hidden">
         <Button
             type="button"
@@ -28,7 +51,7 @@ const Navbar = () => {
             variant="btn_dark_green"
         />
       </div>
-      <Image src="/menu.svg" alt="menu" width={32} height={32} className="inline-block cursor-pointer lg:hidden" />
+      <Image src="/menu.svg" alt="menu" width={32} height={32} className="inline-block cursor-pointer lg:hidden" onClick={() => setIsMenuOpen(!isMenuOpen)} />
     </nav>
   );
 };
