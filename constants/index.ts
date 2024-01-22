df1d7d6a4cf6816c771404e7f564786542d05164
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2024-01-22 12:46:25 -0300

    Update: festures info

--- constants/index.ts
+++ constants/index.ts
@@ -1,86 +1,86 @@
 // NAVIGATION
 export const NAV_LINKS = [
-    { href: '/', key: 'home', label: 'Home' },
-    { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
-    { href: '/', key: 'services', label: 'Services' },
-    { href: '/', key: 'pricing ', label: 'Pricing ' },
-    { href: '/', key: 'contact_us', label: 'Contact Us' },
-  ];
-  
-  // CAMP SECTION
-  export const PEOPLE_URL = [
-    '/person-1.png',
-    '/person-2.png',
-    '/person-3.png',
-    '/person-4.png',
-  ];
-  
-  // FEATURES SECTION
-  export const FEATURES = [
-    {
-      title: 'Real maps can be offline',
-      icon: '/map.svg',
-      variant: 'green',
-      description:
-        'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
-    },
-    {
-      title: 'Set an adventure schedule',
-      icon: '/calendar.svg',
-      variant: 'green',
-      description:
-        "Schedule an adventure with friends. On holidays, there are many interesting offers from Hilink. That way, there's no more discussion",
-    },
-    {
-      title: 'Technology using augment reality',
-      icon: '/tech.svg',
-      variant: 'green',
-      description:
-        'Technology uses augmented reality as a guide to your hiking trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection',
-    },
-    {
-      title: 'Many new locations every month',
-      icon: '/location.svg',
-      variant: 'orange',
-      description:
-        'Lots of new locations every month, because we have a worldwide community of climbers who share their best experiences with climbing',
-    },
-  ];
-  
-  // FOOTER SECTION
-  export const FOOTER_LINKS = [
-    {
-      title: 'Learn More',
-      links: [
-        'About Hilink',
-        'Press Releases',
-        'Environment',
-        'Jobs',
-        'Privacy Policy',
-        'Contact Us',
-      ],
-    },
-    {
-      title: 'Our Community',
-      links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
-    },
-  ];
-  
-  export const FOOTER_CONTACT_INFO = {
-    title: 'Contact Us',
+  { href: "/", key: "home", label: "Home" },
+  { href: "/", key: "how_hilink_work", label: "How Hilink Work?" },
+  { href: "/", key: "services", label: "Services" },
+  { href: "/", key: "pricing ", label: "Pricing " },
+  { href: "/", key: "contact_us", label: "Contact Us" },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  "/person-1.png",
+  "/person-2.png",
+  "/person-3.png",
+  "/person-4.png",
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: "Mapas reais no modo offline",
+    icon: "/map.svg",
+    variant: "green",
+    description:
+      "Oferecemos uma solução para você poder usar nosso aplicativo na hora de escalar, sim mapas offline você pode usar a qualquer momento, mesmo que, não tenha sinal no local",
+  },
+  {
+    title: "Definir um cronograma de aventura",
+    icon: "/calendar.svg",
+    variant: "green",
+    description:
+      "Agende uma aventura com os amigos. Nas férias, há muitas ofertas interessantes da Hilink. Assim, não há mais discussão",
+  },
+  {
+    title: "Tecnologia usando realidade aumentada",
+    icon: "/tech.svg",
+    variant: "green",
+    description:
+      "Usamos a realidade aumentada como um guia para sua trilha na floresta até o topo da montanha. Já suportado pela mais recente tecnologia sem conexão à Internet",
+  },
+  {
+    title: "Muitos novos locais todos os meses",
+    icon: "/location.svg",
+    variant: "orange",
+    description:
+      "Muitos locais novos todos os meses, porque temos uma comunidade mundial de escaladores que partilham as suas melhores experiências com escalada",
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: "Learn More",
     links: [
-      { label: 'Admin Officer', value: '123-456-7890' },
-      { label: 'Email Officer', value: 'hilink@akinthil.com' },
+      "About Hilink",
+      "Press Releases",
+      "Environment",
+      "Jobs",
+      "Privacy Policy",
+      "Contact Us",
     ],
-  };
-  
-  export const SOCIALS = {
-    title: 'Social',
-    links: [
-      '/facebook.svg',
-      '/instagram.svg',
-      '/twitter.svg',
-      '/youtube.svg',
-      '/wordpress.svg',
-    ],
-  };
+  },
+  {
+    title: "Our Community",
+    links: ["Climbing xixixi", "Hiking hilink", "Hilink kinthill"],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: "Contact Us",
+  links: [
+    { label: "Admin Officer", value: "123-456-7890" },
+    { label: "Email Officer", value: "hilink@akinthil.com" },
+  ],
+};
+
+export const SOCIALS = {
+  title: "Social",
+  links: [
+    "/facebook.svg",
+    "/instagram.svg",
+    "/twitter.svg",
+    "/youtube.svg",
+    "/wordpress.svg",
+  ],
+};
